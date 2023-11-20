53edf1f1bdeb0212b2d9c91f8e042f0156ef458c
leco888 <47178265+leco888@users.noreply.github.com>
2023-11-20 19:08:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, redirect, url_for, render_template
 import os
 from werkzeug.utils import secure_filename
@@ -22,14 +21,25 @@ def index():
 @app.route('/add_player', methods=['POST'])
 def add_player():
     # Add player logic including image upload handling
-    # ...
+    player_name = request.form['name']
+    if 'image' in request.files:
+        file = request.files['image']
+        if file.filename != '' and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            players[player_name] = {'image': url_for('static', filename=f'uploads/{filename}')}
+    return redirect(url_for('index'))
 
 @app.route('/remove_player', methods=['POST'])
 def remove_player():
     # Remove player logic
-    # ...
+    player_name = request.form['name']
+    if player_name in players:
+        players.pop(player_name)
+    return redirect(url_for('index'))
 
 # Add other routes as necessary
 
 if __name__ == '__main__':
     app.run(debug=True)
+
