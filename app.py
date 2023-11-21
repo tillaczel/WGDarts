948cb3a4c5b16497e91864077dadd425e0a9ec95
till <till.aczel@gmail.com>
2023-11-21 19:26:54 +0100

    show players

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Dictionary to store player data
-players = {}
+players = utils.load_players().to_dict(orient='records')
+print(players)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
