570986d087d84960fbe847b518c0e341dc8f5da9
Leandro Costanzo <lcostanzo@live.de>
2023-11-21 18:31:15 +0100

    wip

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, url_for, render_template
 import os
 from werkzeug.utils import secure_filename
+import utils
 
 # Configure your application and upload folder
 app = Flask(__name__)
@@ -20,14 +21,10 @@ def index():
 
 @app.route('/add_player', methods=['POST'])
 def add_player():
-    # Add player logic including image upload handling
     player_name = request.form['name']
-    if 'image' in request.files:
-        file = request.files['image']
-        if file.filename != '' and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            players[player_name] = {'image': url_for('static', filename=f'uploads/{filename}')}
+    image_path = utils.add_player(player_name)
+    file = request.files['image']
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], image_path))
     return redirect(url_for('index'))
 
 @app.route('/remove_player', methods=['POST'])
